2081726d200c2591c9793c39be2c2243888a6614
Umang Choudhary <uchoudhary@reddyice.com>
2022-08-23 12:12:22 +0530

    fix kotlin debug error.

--- app/src/main/java/com/saifi369/databindingexample/utils/StringUtils.kt
+++ app/src/main/java/com/saifi369/databindingexample/utils/StringUtils.kt
@@ -4,10 +4,12 @@ class StringUtils {
 
     companion object {
 
+        @JvmStatic
         fun getTotalRatings(totalRatings: Int): String {
             return "($totalRatings)"
         }
 
+        @JvmStatic
         fun getPrice(price: Double): String {
             return "$price$"
         }
