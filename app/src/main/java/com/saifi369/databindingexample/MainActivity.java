f19bdaeca9a9e80bc4b9813ce4f6a6b7695608e0
Umang Choudhary <uchoudhary@reddyice.com>
2022-08-23 21:56:44 +0530

    Enhancement

--- app/src/main/java/com/saifi369/databindingexample/MainActivity.java
+++ app/src/main/java/com/saifi369/databindingexample/MainActivity.java
@@ -1,15 +1,9 @@
 package com.saifi369.databindingexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.saifi369.databindingexample.databinding.MainActivityBinding;
-import com.saifi369.databindingexample.model.User;
-
-import org.w3c.dom.Text;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,8 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView1=findViewById(R.id.tv_first);
-        TextView textView2=findViewById(R.id.tv_second);
+        TextView textView1 = findViewById(R.id.tv_first);
+        TextView textView2 = findViewById(R.id.tv_second);
 
         textView1.setText("This is first text");
         textView1.setText("This is second text");
