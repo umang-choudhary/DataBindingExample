24cdfe603f51626f5ee51142b6a9115f3aa9401d
Umang Choudhary <uchoudhary@reddyice.com>
2022-08-23 17:46:06 +0530

    Make product parcelable.

--- app/src/main/java/com/saifi369/databindingexample/model/Product.kt
+++ app/src/main/java/com/saifi369/databindingexample/model/Product.kt
@@ -1,20 +1,24 @@
 package com.saifi369.databindingexample.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class Product(
-        val itemId: String,
-        val name: String,
-        val description: String,
-        val price: Double,
-        val salePrice: Double,
-        val rating: Float,
-        val totalRatings: Int,
-        val image: String,
-        val errorImage: Int,
-        val placeholderImage: Int) {
+    val itemId: String,
+    val name: String,
+    val description: String,
+    val price: Double,
+    val salePrice: Double,
+    val rating: Float,
+    val totalRatings: Int,
+    val image: String,
+    val errorImage: Int,
+    val placeholderImage: Int
+) : Parcelable {
 
     val priceMap = mapOf<String, Double>(
-            "price" to price,
-            "sale_price" to salePrice
+        "price" to price,
+        "sale_price" to salePrice
     )
 }
